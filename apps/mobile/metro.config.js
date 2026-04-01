ca3f83007c64e53c548ebccf4ed4182c950fb85c
zhenzijun <shenmegm@gmail.com>
2026-04-01 23:36:48 +0800

    1

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -2,7 +2,4 @@ const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
 
-config.watchFolders = [require("path").resolve(__dirname, "../..")];
-
 module.exports = config;
-
